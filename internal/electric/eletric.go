36222a962163a88de8c3aa17dfd911a198a9e2eb
Anh Cao <anhcao4922@gmail.com>
2024-01-21 22:48:23 +0200

    Create data structs to represent electric data
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

---
+++ internal/electric/eletric.go
@@ -0,0 +1,15 @@
+package electric
+
+const (
+	BASE_URL   = "https://oomi.fi/wp-json"
+	SPOT_PRICE = "spot-price"
+	GET_V1     = "v1/get"
+)
+
+func FetchSpotPrice(requestParameters PriceRequestParameters) {
+
+}
+
+func formatRequestParameters(requestParameters PriceRequestParameters) (endPoint string, err error) {
+	return
+}
--- internal/electric/models.go
+++ internal/electric/models.go
@@ -1,1 +1,37 @@
 package electric
+
+import "time"
+
+type TimelyPrice struct {
+	OriginalTime time.Time `json:"orig_time"`
+	Time         time.Time `json:"time"`
+	Price        float64   `json:"price"`
+	VatFactor    float64   `json:"vat_factor"`
+	IsToday      bool      `json:"isToday"`
+}
+
+type PriceGroup struct {
+	Name string        `json:"name"`
+	Data []TimelyPrice `json:"data"`
+}
+
+// Group represents 'hour', 'day', week', 'month', 'year'
+type PriceData struct {
+	Group  string       `json:"group"`
+	Series []PriceGroup `json:"series"`
+}
+
+type PriceResponse struct {
+	Status string    `json:"status"`
+	Data   PriceData `json:"data"`
+}
+
+// vatIncluded will affect when it is different than 0
+type PriceRequestParameters struct {
+	StartDate         time.Time `json:"starttime"`
+	EndDate           time.Time `json:"endtime"`
+	Marginal          float64   `json:"margin"`
+	Group             string    `json:"group"`
+	VatIncluded       int32     `json:"include_vat"`
+	CompareToLastYear int32     `json:"compare_to_last_year"`
+}
