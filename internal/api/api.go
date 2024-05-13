2418e77d0e43e3c611dea7779ae56990bfbd1478
Anh Cao <anhcao4922@gmail.com>
2024-05-14 00:37:59 +0300

    implement the endpoint to fetch today and tomorrow price. Put some todo comments
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/api/api.go
+++ internal/api/api.go
@@ -24,11 +24,11 @@ func PostMarketPrice(w http.ResponseWriter, r *http.Request) {
 	externalData, errorType, err := electric.FetchSpotPrice(reqBody)
 	if err != nil {
 		if errorType == electric.SERVER_ERROR {
-			logger.Logger.Error("failed to fetch data", zap.Error(err))
+			logger.Logger.Error("[server error] failed to fetch data", zap.Error(err))
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		logger.Logger.Error("failed to fetch data", zap.Error(err))
+		logger.Logger.Error("[request error] failed to fetch data", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
@@ -46,7 +46,33 @@ func PostMarketPrice(w http.ResponseWriter, r *http.Request) {
 // If tomorrow's price is not available yet, return empty struct.
 // Then client needs to show readable information to indicate that data is not available yet.
 func GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
+	var reqBody, err = electric.BuildTodayTomorrowAsBodyRequest()
+	if err != nil {
+		logger.Logger.Error("failed to build request body", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
 	w.Header().Set("Content-Type", "application/json")
+	todayTomorrowPrice, errorType, err := electric.FetchSpotPrice(reqBody)
+	if err != nil {
+		if errorType == electric.SERVER_ERROR {
+			logger.Logger.Error("[server error] failed to fetch data", zap.Error(err))
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		logger.Logger.Error("[request error] failed to fetch data", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(todayTomorrowPrice); err != nil {
+		logger.Logger.Error("failed to encode response data", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	// todo: maybe good to have a handling to split the response from list of array into 2 struct Today and Tomorrow. By doing this, client can easily visualize the view
+	logger.Logger.Info("get today and tomorrow's exchange price successfully")
 
 }
