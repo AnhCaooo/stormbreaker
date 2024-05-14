83c7486faff6b5632751e6963ada8c5c24953846
Anh Cao <anhcao4922@gmail.com>
2024-05-15 01:30:54 +0300

    feature(/today-tomorrow api): map the external response to custom data struct
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/api/api.go
+++ internal/api/api.go
@@ -66,13 +66,19 @@ func GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if err := json.NewEncoder(w).Encode(todayTomorrowPrice); err != nil {
+	todayTomorrowResponse, err := electric.MapToTodayTomorrowResponse(todayTomorrowPrice)
+	if err != nil {
+		logger.Logger.Error("[server error] failed to map to informative struct data", zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(todayTomorrowResponse); err != nil {
 		logger.Logger.Error("failed to encode response data", zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	// todo: maybe good to have a handling to split the response from list of array into 2 struct Today and Tomorrow. By doing this, client can easily visualize the view
 	logger.Logger.Info("get today and tomorrow's exchange price successfully")
 
 }
