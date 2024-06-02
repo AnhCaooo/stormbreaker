8e58d734ba6ea3b439c179129529f5122417ebcb
Anh Cao <anhcao4922@gmail.com>
2024-06-02 19:14:38 +0300

    refactor and split way to fetch current spot price
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/api/api.go
+++ internal/api/api.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/AnhCaooo/stormbreaker/internal/cache"
 	"github.com/AnhCaooo/stormbreaker/internal/electric"
-	"github.com/AnhCaooo/stormbreaker/internal/helpers"
 	"github.com/AnhCaooo/stormbreaker/internal/logger"
 	"github.com/AnhCaooo/stormbreaker/internal/models"
 	"go.uber.org/zap"
@@ -64,38 +63,6 @@ func GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var reqBody, err = helpers.BuildTodayTomorrowAsBodyRequest()
-	if err != nil {
-		logger.Logger.Error("[server error] failed to build request body", zap.Error(err))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	todayTomorrowPrice, errorType, err := electric.FetchSpotPrice(reqBody)
-	if err != nil {
-		if errorType == models.SERVER_ERROR {
-			logger.Logger.Error("[server error] failed to fetch data", zap.Error(err))
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		logger.Logger.Error("[request error] failed to fetch data", zap.Error(err))
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
-
-	todayTomorrowResponse, err := helpers.MapToTodayTomorrowResponse(todayTomorrowPrice)
-	if err != nil {
-		logger.Logger.Error("[server error] failed to map to informative struct data", zap.Error(err))
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if err := json.NewEncoder(w).Encode(todayTomorrowResponse); err != nil {
-		logger.Logger.Error("failed to encode response data", zap.Error(err))
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	todayTomorrowResponse := electric.FetchCurrentSpotPrice(w)
 	cache.Cache.SetExpiredAt(cacheKey, &todayTomorrowResponse, expiredAtHour)
-	logger.Logger.Info("get today and tomorrow's exchange price successfully")
-
 }
