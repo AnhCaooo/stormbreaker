1fb7644eebf292d9110ff19755f5049f4132a308
Anh Cao <anhcao4922@gmail.com>
2024-11-24 22:47:16 +0200

    feat: implement CRUD operation in Mongo for userSettings
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -10,6 +10,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.uber.org/zap"
 )
 
 var Collection *mongo.Collection
@@ -19,12 +20,12 @@ func Init(ctx context.Context, cfg models.Database) (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI(getURI(cfg))
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
-		return nil, fmt.Errorf("failed to connect to database. Error: %s", err.Error())
+		return nil, fmt.Errorf("failed to connect to database: %s", err.Error())
 	}
 
 	err = client.Ping(ctx, nil)
 	if err != nil {
-		return nil, fmt.Errorf("failed to ping database. Error: %s", err.Error())
+		return nil, fmt.Errorf("failed to ping database: %s", err.Error())
 	}
 
 	Collection = client.Database(cfg.Name).Collection(cfg.Collection)
@@ -52,8 +53,9 @@ func GetPriceSettings(ctx context.Context, userID string) (*models.PriceSettings
 	var settings models.PriceSettings
 	err := Collection.FindOne(ctx, filter).Decode(&settings)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get price setting: %s", err.Error())
 	}
+	logger.Logger.Info("get price settings successfully", zap.Any("user_id", userID))
 	return &settings, nil
 }
 
@@ -61,7 +63,19 @@ func GetPriceSettings(ctx context.Context, userID string) (*models.PriceSettings
 //
 // Use case: update the price settings for specific user
 func PatchPriceSettings(ctx context.Context, settings models.PriceSettings) error {
+	filter := bson.M{"user_id": settings.UserID}
 
+	updates := bson.M{
+		"$set": bson.M{
+			"vat_included": settings.VatIncluded,
+			"margin":       settings.Marginal,
+		},
+	}
+	result, err := Collection.UpdateOne(ctx, filter, updates)
+	if err != nil {
+		return fmt.Errorf("failed to update price settings: %s", err.Error())
+	}
+	logger.Logger.Info("update price settings successfully", zap.Any("updated_amount", result.MatchedCount))
 	return nil
 }
 
@@ -69,5 +83,12 @@ func PatchPriceSettings(ctx context.Context, settings models.PriceSettings) erro
 //
 // Use case: delete the price settings if user is deleted
 func DeletePriceSettings(ctx context.Context, userID string) error {
+	filter := bson.M{"user_id": userID}
+
+	result, err := Collection.DeleteOne(ctx, filter)
+	if err != nil {
+		return fmt.Errorf("failed to delete price settings. Error: %s", err.Error())
+	}
+	logger.Logger.Info("delete user price settings successfully", zap.Int64("deleted_amount", result.DeletedCount))
 	return nil
 }
