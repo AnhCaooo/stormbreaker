cefa8e021be9a4b20f40fb8c5a301daaccbc22ee
Anh Cao <anhcao4922@gmail.com>
2024-11-25 22:31:10 +0200

    feat: implement CreatePriceSettings

--- internal/api/handlers/priceSettings.go
+++ internal/api/handlers/priceSettings.go
@@ -1,8 +1,16 @@
 package handlers
 
 import (
+	"context"
 	"fmt"
 	"net/http"
+
+	"github.com/AnhCaooo/go-goods/encode"
+	"github.com/AnhCaooo/stormbreaker/internal/constants"
+	"github.com/AnhCaooo/stormbreaker/internal/db"
+	"github.com/AnhCaooo/stormbreaker/internal/logger"
+	"github.com/AnhCaooo/stormbreaker/internal/models"
+	"go.uber.org/zap"
 )
 
 // GetPriceSettings retrieves the price settings for specified user
@@ -12,7 +20,27 @@ func GetPriceSettings(w http.ResponseWriter, r *http.Request) {
 
 // CreatePriceSettings creates a new price settings for new user
 func CreatePriceSettings(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "pong")
+	reqBody, err := encode.DecodeRequest[models.PriceSettings](r)
+	if err != nil {
+		logger.Logger.Error(constants.Client, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := db.InsertPriceSettings(context.TODO(), reqBody); err != nil {
+		logger.Logger.Error(constants.Server, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := encode.EncodeResponse(w, http.StatusCreated, reqBody); err != nil {
+		logger.Logger.Error(
+			fmt.Sprintf("%s failed to encode response body:", constants.Server),
+			zap.Error(err),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // PatchPriceSettings updates the price settings for specified user
