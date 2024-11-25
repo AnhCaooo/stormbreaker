e4b9855bf3e68d1937db495bdd7e1375b3540371
Anh Cao <anhcao4922@gmail.com>
2024-11-25 22:43:57 +0200

    feat: implement Patch and Delete price settings

--- internal/api/handlers/priceSettings.go
+++ internal/api/handlers/priceSettings.go
@@ -67,10 +67,50 @@ func CreatePriceSettings(w http.ResponseWriter, r *http.Request) {
 
 // PatchPriceSettings updates the price settings for specified user
 func PatchPriceSettings(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "pong")
+	reqBody, err := encode.DecodeRequest[models.PriceSettings](r)
+	if err != nil {
+		logger.Logger.Error(constants.Client, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := db.PatchPriceSettings(context.TODO(), reqBody); err != nil {
+		logger.Logger.Error(constants.Server, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := encode.EncodeResponse(w, http.StatusCreated, reqBody); err != nil {
+		logger.Logger.Error(
+			fmt.Sprintf("%s failed to encode response body:", constants.Server),
+			zap.Error(err),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 // DeletePriceSettings deletes the price settings when user was deleted or removed
 func DeletePriceSettings(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "pong")
+	reqBody, err := encode.DecodeRequest[string](r)
+	if err != nil {
+		logger.Logger.Error(constants.Client, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if err := db.DeletePriceSettings(context.TODO(), reqBody); err != nil {
+		logger.Logger.Error(constants.Server, zap.Error(err))
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if err := encode.EncodeResponse(w, http.StatusOK, reqBody); err != nil {
+		logger.Logger.Error(
+			fmt.Sprintf("%s failed to encode response body:", constants.Server),
+			zap.Error(err),
+		)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
