430ef585c63676a8242186607ddc32de6d8010b3
Anh Cao <anhcao4922@gmail.com>
2024-11-26 21:19:16 +0200

    bugfix: add and improve error message
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/db/mongo_test.go
+++ internal/db/mongo_test.go
@@ -1,2 +1,11 @@
 // AnhCao 2024
 package db
+
+// create new user settings -> get user settings
+// create new duplicate user and get error
+
+// update user settings
+// update non-existing user settings and get error
+
+// delete existing user settings
+// delete non existing user settings and get error
