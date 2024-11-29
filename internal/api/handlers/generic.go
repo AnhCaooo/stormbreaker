3f6992eb449a250697a7565586fef80fa1c63491
Anh Cao <anhcao4922@gmail.com>
2024-11-29 18:56:23 +0200

    feat: create handler instance

--- internal/api/handlers/generic.go
+++ internal/api/handlers/generic.go
@@ -5,25 +5,45 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/AnhCaooo/stormbreaker/internal/logger"
+	"github.com/AnhCaooo/stormbreaker/internal/db"
+	"github.com/AnhCaooo/stormbreaker/internal/models"
 	"go.uber.org/zap"
 )
 
+type Handler struct {
+	logger *zap.Logger
+	cache  *models.Cache
+	mongo  *db.Mongo
+}
+
+func NewHandler(logger *zap.Logger, cache *models.Cache, mongo *db.Mongo) *Handler {
+	return &Handler{
+		logger: logger,
+		cache:  cache,
+		mongo:  mongo,
+	}
+}
+
 // return response when request url is not found
-func NotFound(w http.ResponseWriter, r *http.Request) {
+func (h Handler) NotFound(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
-	logger.Logger.Info("undefined endpoint", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	h.logger.Info("undefined endpoint", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
 	w.Write([]byte("404 - Not found"))
 }
 
 // return response when request method is not allowed
-func NotAllowed(w http.ResponseWriter, r *http.Request) {
+func (h Handler) NotAllowed(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusMethodNotAllowed)
-	logger.Logger.Info("method not allowed", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	h.logger.Info("method not allowed", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
 	w.Write([]byte("405 - Method not allowed"))
 }
 
 // Ping the connection to the server
-func Ping(w http.ResponseWriter, r *http.Request) {
+func (h Handler) Ping(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong")
+}
+
+func (h Handler) Logger(next http.Handler, w http.ResponseWriter, r *http.Request) {
+	h.logger.Info("request received", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+	next.ServeHTTP(w, r)
 }
--- internal/api/handlers/marketPrice.go
+++ internal/api/handlers/marketPrice.go
@@ -6,38 +6,36 @@ import (
 	"time"
 
 	"github.com/AnhCaooo/go-goods/encode"
-	"github.com/AnhCaooo/stormbreaker/internal/cache"
 	"github.com/AnhCaooo/stormbreaker/internal/constants"
 	"github.com/AnhCaooo/stormbreaker/internal/electric"
 	"github.com/AnhCaooo/stormbreaker/internal/helpers"
-	"github.com/AnhCaooo/stormbreaker/internal/logger"
 	"github.com/AnhCaooo/stormbreaker/internal/models"
 	"go.uber.org/zap"
 )
 
 // Fetch the market spot price of electric in Finland in any times
-func PostMarketPrice(w http.ResponseWriter, r *http.Request) {
+func (h Handler) PostMarketPrice(w http.ResponseWriter, r *http.Request) {
 	reqBody, err := encode.DecodeRequest[models.PriceRequest](r)
 	if err != nil {
-		logger.Logger.Error(constants.Client, zap.Error(err))
+		h.logger.Error(constants.Client, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
+	electric := electric.NewElectric(h.logger)
 	externalData, errorType, err := electric.FetchSpotPrice(reqBody)
 	if err != nil {
 		if errorType == models.SERVER_ERROR {
-			logger.Logger.Error(constants.Server, zap.Error(err))
+			h.logger.Error(constants.Server, zap.Error(err))
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		logger.Logger.Error(constants.Client, zap.Error(err))
+		h.logger.Error(constants.Client, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
 	if err := encode.EncodeResponse(w, http.StatusOK, externalData); err != nil {
-		logger.Logger.Error(
+		h.logger.Error(
 			fmt.Sprintf("%s failed to encode data from external source", constants.Server),
 			zap.Error(err),
 		)
@@ -45,32 +43,33 @@ func PostMarketPrice(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	logger.Logger.Info("get market price of electric successfully")
+	h.logger.Info("get market price of electric successfully")
 }
 
 // Fetch and return the exchange price for today and tomorrow.
 // If tomorrow's price is not available yet, return empty struct.
 // Then client needs to show readable information to indicate that data is not available yet.
-func GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
+func (h Handler) GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
 	cacheKey := "today-tomorrow-exchange-price"
 
-	cachePrice, isValid := cache.Cache.Get(cacheKey)
+	cachePrice, isValid := h.cache.Get(cacheKey)
 	if isValid {
 		if err := encode.EncodeResponse(w, http.StatusOK, cachePrice); err != nil {
-			logger.Logger.Error(
+			h.logger.Error(
 				fmt.Sprintf("%s failed to encode cache data", constants.Server),
 				zap.Error(err),
 			)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		logger.Logger.Info("[cache] get today and tomorrow's exchange price successfully")
+		h.logger.Info("[cache] get today and tomorrow's exchange price successfully")
 		return
 	}
 
+	electric := electric.NewElectric(h.logger)
 	todayTomorrowResponse, err := electric.FetchCurrentSpotPrice(w)
 	if err != nil {
-		logger.Logger.Error(
+		h.logger.Error(
 			fmt.Sprintf("%s failed to fetch today and/or tomorrow spot price from external source", constants.Server),
 			zap.Error(err),
 		)
@@ -83,25 +82,25 @@ func GetTodayTomorrowPrice(w http.ResponseWriter, r *http.Request) {
 	if todayTomorrowResponse.Tomorrow.Available {
 		expiredTime, err := helpers.SetTime(23, 59)
 		if err != nil {
-			logger.Logger.Error(
+			h.logger.Error(
 				fmt.Sprintf("%s failed to set expired time for caching", constants.Server),
 				zap.Error(err),
 			)
 			return
 		}
-		cache.Cache.SetExpiredAtTime(cacheKey, &todayTomorrowResponse, expiredTime)
+		h.cache.SetExpiredAtTime(cacheKey, &todayTomorrowResponse, expiredTime)
 		return
 	}
 	// if tomorrow price is not available and sending request time is before 14:00, then cache until 14:00
 	expiredTime, err := helpers.SetTime(14, 00)
 	if err != nil {
-		logger.Logger.Error(
+		h.logger.Error(
 			fmt.Sprintf("%s failed to set expired time for caching", constants.Server),
 			zap.Error(err),
 		)
 		return
 	}
 	if time.Now().Before(expiredTime) {
-		cache.Cache.SetExpiredAtTime(cacheKey, &todayTomorrowResponse, expiredTime)
+		h.cache.SetExpiredAtTime(cacheKey, &todayTomorrowResponse, expiredTime)
 	}
 }
