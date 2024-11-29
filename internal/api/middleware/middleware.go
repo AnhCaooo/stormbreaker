11a378e4e8424c8881d861fe36a37e1936f638d7
Anh Cao <anhcao4922@gmail.com>
2024-11-29 18:57:26 +0200

    feat: create middleware instance and improve

--- internal/api/middleware/middleware.go
+++ internal/api/middleware/middleware.go
@@ -6,35 +6,46 @@ import (
 	"strings"
 
 	"github.com/AnhCaooo/go-goods/auth"
-	"github.com/AnhCaooo/stormbreaker/internal/config"
-	"github.com/AnhCaooo/stormbreaker/internal/logger"
+	"github.com/AnhCaooo/stormbreaker/internal/models"
 	"go.uber.org/zap"
 )
 
+type Middleware struct {
+	logger *zap.Logger
+	config *models.Config
+}
+
+func NewMiddleware(logger *zap.Logger, config *models.Config) *Middleware {
+	return &Middleware{
+		logger: logger,
+		config: config,
+	}
+}
+
 // log the coming request to the server
-func Logger(next http.Handler) http.Handler {
+func (m *Middleware) Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		logger.Logger.Info("request received", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
+		m.logger.Info("request received", zap.String("method", r.Method), zap.String("endpoint", r.URL.Path))
 		next.ServeHTTP(w, r)
 	})
 }
 
 // read the token from request and do verify the access token
-func Authenticate(next http.Handler) http.Handler {
+func (m *Middleware) Authenticate(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		tokenString := r.Header.Get("Authorization")
 		if tokenString == "" {
 			w.WriteHeader(http.StatusForbidden)
-			logger.Logger.Info("permission Denied: No token provided")
+			m.logger.Info("permission Denied: No token provided")
 			w.Write([]byte("403 - Forbidden"))
 			return
 		}
 
 		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
-		err := auth.VerifyToken(tokenString, config.Config.Supabase.Auth.JwtSecret)
+		err := auth.VerifyToken(tokenString, m.config.Supabase.Auth.JwtSecret)
 		if err != nil {
 			w.WriteHeader(http.StatusUnauthorized)
-			logger.Logger.Error("unauthorized request", zap.Error(err))
+			m.logger.Error("unauthorized request", zap.Error(err))
 			w.Write([]byte("401 - Unauthorized"))
 			return
 		}
