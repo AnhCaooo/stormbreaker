9300166299e6a8bb35cbde0bdc7be77af7d24ee2
Anh Cao <anhcao4922@gmail.com>
2024-12-07 16:50:17 +0200

    fix: read userId from accessToken
    Signed-off-by: Anh Cao <anhcao4922@gmail.com>

--- internal/api/handlers/priceSettings.go
+++ internal/api/handlers/priceSettings.go
@@ -10,11 +10,12 @@ import (
 	"go.uber.org/zap"
 )
 
+// todo: cache the price settings to improve performance
 // GetPriceSettings retrieves the price settings for specified user
 func (h Handler) GetPriceSettings(w http.ResponseWriter, r *http.Request) {
 	userid, ok := r.Context().Value(constants.UserIdKey).(string)
 	if !ok {
-		http.Error(w, "User ID not found in context", http.StatusInternalServerError)
+		http.Error(w, "User ID not found in context", http.StatusBadRequest)
 		return
 	}
 
@@ -38,10 +39,22 @@ func (h Handler) GetPriceSettings(w http.ResponseWriter, r *http.Request) {
 
 // CreatePriceSettings creates a new price settings for new user
 func (h Handler) CreatePriceSettings(w http.ResponseWriter, r *http.Request) {
+	userId, ok := r.Context().Value(constants.UserIdKey).(string)
+	if !ok {
+		http.Error(w, "User ID not found in context", http.StatusBadRequest)
+		return
+	}
+
 	reqBody, err := encode.DecodeRequest[models.PriceSettings](r)
 	if err != nil {
 		h.logger.Error(constants.Client, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	// Patch userID to price settings
+	reqBody.UserID = userId
+	if reqBody.UserID != "" {
+		http.Error(w, "cannot insert un-authenticated document", http.StatusBadRequest)
 		return
 	}
 
@@ -67,10 +80,21 @@ func (h Handler) CreatePriceSettings(w http.ResponseWriter, r *http.Request) {
 
 // PatchPriceSettings updates the price settings for specified user
 func (h Handler) PatchPriceSettings(w http.ResponseWriter, r *http.Request) {
+	userId, ok := r.Context().Value(constants.UserIdKey).(string)
+	if !ok {
+		http.Error(w, "User ID not found in context", http.StatusBadRequest)
+		return
+	}
+
 	reqBody, err := encode.DecodeRequest[models.PriceSettings](r)
 	if err != nil {
 		h.logger.Error(constants.Client, zap.Error(err))
 		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	reqBody.UserID = userId
+	if reqBody.UserID != "" {
+		http.Error(w, "cannot insert un-authenticated document", http.StatusBadRequest)
 		return
 	}
 
@@ -94,11 +118,12 @@ func (h Handler) PatchPriceSettings(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// todo: maybe only Admin can perform this action? (to be considered)
 // DeletePriceSettings deletes the price settings when user was deleted or removed
 func (h Handler) DeletePriceSettings(w http.ResponseWriter, r *http.Request) {
 	userId, ok := r.Context().Value(constants.UserIdKey).(string)
 	if !ok {
-		http.Error(w, "User ID not found in context", http.StatusInternalServerError)
+		http.Error(w, "User ID not found in context", http.StatusBadRequest)
 		return
 	}
 
